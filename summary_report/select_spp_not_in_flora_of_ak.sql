3f1806f5e4059913d7eba70bbd0a82f055c36da6
awells <awells@abrinc.com>
2022-09-14 09:20:44 -0700

    minor changes

--- summary_report/select_spp_not_in_flora_of_ak.sql
+++ summary_report/select_spp_not_in_flora_of_ak.sql
@@ -9,6 +9,7 @@ LEFT JOIN bioblitz.ak_bioblitz_taxonomy_crossref_2022_view USING (scientific_nam
 WHERE name_accepted NOT IN ('NO MATCH')
 	  )
 	  
+--names in this list don't have a match in the taxon_accepted table and needed to added there and to all related tables
 SELECT * FROM j
 LEFT JOIN public.flora_of_alaska_view USING (name_accepted)
 WHERE accepted_id IS NULL
