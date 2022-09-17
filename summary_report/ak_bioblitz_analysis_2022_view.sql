905855f61efc892218e893236c3c7425398f499e
awells <awells@abrinc.com>
2022-09-17 07:54:15 -0700

    updated view to include research and non-research grade observations, and added create view SQL

--- summary_report/ak_bioblitz_analysis_2022_view.sql
+++ summary_report/ak_bioblitz_analysis_2022_view.sql
@@ -1,3 +1,8 @@
+-- View: bioblitz.ak_bioblitz_analysis_2022_view
+
+-- DROP VIEW bioblitz.ak_bioblitz_analysis_2022_view;
+
+CREATE OR REPLACE VIEW bioblitz.ak_bioblitz_analysis_2022_view AS
 WITH d AS (SELECT id, bbcrv.name_accepted, level, habit, native, non_native, state_rank, global_rank, federal_listing,
 		observed_on_string, observed_on, time_observed_at, time_zone, user_id, user_login, created_at, updated_at, quality_grade, license, url, image_url, sound_url, tag_list, description, 
 		num_identification_agreements, num_identification_disagreements, captive_cultivated, oauth_application_id, place_guess, 
@@ -15,10 +20,13 @@ WITH d AS (SELECT id, bbcrv.name_accepted, level, habit, native, non_native, sta
 	FROM bioblitz.ak_bioblitz_import AS bb
 	JOIN bioblitz.ak_bioblitz_taxonomy_crossref_2022_view AS bbcrv USING (scientific_name)
 	LEFT JOIN public.flora_of_alaska_view AS foav USING (name_accepted)
-	WHERE quality_grade = 'research' AND name_accepted NOT IN ('NO MATCH') --6741 records meet this criteria
+	WHERE latitude IS NOT NULL AND longitude IS NOT NULL
+		   --quality_grade = 'research' AND name_accepted NOT IN ('NO MATCH') --6741 records meet this criteria
 		--AND coordinates_obscured IS TRUE
 		--AND private_latitude IS NOT NULL
 		ORDER BY name_accepted)
+
+--SELECT * FROM d WHERE quality_grade NOT IN ('research') OR name_accepted = 'NO MATCH' --2841 record either not research grade and/or no match in Flora of AK Checklist
 
 SELECT row_number() OVER (ORDER BY id) AS ogc_fid,id, name_accepted, level, habit, native, non_native, state_rank, global_rank, federal_listing, st_setsrid(st_makepoint(longitude, latitude), 4326) AS geom,
 		observed_on_string, observed_on, time_observed_at, time_zone, user_id, user_login, created_at, updated_at, quality_grade, license, url, image_url, sound_url, tag_list, description, 
@@ -50,3 +58,11 @@ Added to flora of AK taxon accepted table and child tables
 'Ulva intestinalis'
 'Trentepohlia aurea'
 */
+
+;
+
+ALTER TABLE bioblitz.ak_bioblitz_analysis_2022_view
+    OWNER TO aaronwells;
+COMMENT ON VIEW bioblitz.ak_bioblitz_analysis_2022_view
+    IS 'This view standardizes the taxonomic names, includes all observations, including research and non-research grade and 4 records of taxa that did not match a taxon in the Flora of Alaska provisional checklist,  replaces the public lat/long with private, and creates geom.';
+
